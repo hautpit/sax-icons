34b95c0f06667107eb5d1fb53ccf62053e262287
HauTP <hautp1@vntt.com.vn>
2022-03-07 11:35:13 +0700

    export

---
+++ .next/static/3U5LBwcK0JfnOqD8tuJP-/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/149b18973e5508c7.css","static/chunks/pages/index-c6af4a434b3690fe.js"],"/_error":["static/chunks/pages/_error-2f883067a14f4c4a.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
